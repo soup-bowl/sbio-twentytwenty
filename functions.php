2f07c58b5d2b00ae580fd83be37ab7680ddd76c9
soup-bowl <code@soupbowl.io>
2020-03-02 17:38:47 +0000

    Change GB editor to match my changes.

--- functions.php
+++ functions.php
@@ -37,3 +37,12 @@ add_action(
 		wp_enqueue_script( 'soupbowl-login' );
 	}
 );
+
+add_action(
+	'after_setup_theme',
+	function() {
+		add_theme_support( 'editor-styles' );
+
+		add_editor_style( 'editor-style.css' );
+	}
+);
